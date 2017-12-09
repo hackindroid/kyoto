51f03fb1a321734ea196b56a7f49d817b4d4441c
hackindroid <hackindroid98@gmail.com>
2017-12-09 20:58:25 +0530

    tab Layout Addition
    Signed-off-by: hackindroid <hackindroid98@gmail.com>

--- app/src/main/java/com/hackindroid/kyoto/MainActivity.java
+++ app/src/main/java/com/hackindroid/kyoto/MainActivity.java
@@ -3,13 +3,44 @@ package com.hackindroid.kyoto;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.hackindroid.kyoto.fragments.AdvFragment;
+import com.hackindroid.kyoto.fragments.BooksFragment;
+import com.hackindroid.kyoto.fragments.NotesFragment;
+import com.hackindroid.kyoto.fragments.PracticalsFragment;
+import com.hackindroid.kyoto.fragments.QPaperFragment;
+import com.hackindroid.kyoto.fragments.ResourceFragment;
+import com.hackindroid.kyoto.fragments.SettingsFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    FragmentManager fragmentManager;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    Fragment fragment;
+    LinearLayout mainLayout;
+//    FrameLayout flMain;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +49,27 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    tabLayout.setVisibility(View.VISIBLE);
+                    viewPager.setVisibility(View.VISIBLE);
+                    fragmentManager.beginTransaction()
+                            .remove(fragment)
+                            .commit();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    fragment = new AdvFragment();
+                    tabLayout.setVisibility(GONE);
+                    viewPager.setVisibility(GONE);
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.main_layout, fragment)
+                            .commit();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    tabLayout.setVisibility(GONE);
+                    viewPager.setVisibility(GONE);
+                    fragment = new SettingsFragment();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.main_layout, fragment)
+                            .commit();
                     return true;
             }
             return false;
@@ -36,8 +81,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        //fragment manager
+        fragmentManager = getSupportFragmentManager();
+
+        //Relative Layout
+        mainLayout = findViewById(R.id.main_layout);
+//        flMain = findViewById(R.id.fl_main);
+
+//        fragmentManager.beginTransaction()
+//                .replace(R.id.main_layout, new ResourceFragment())
+//                .commit();
+
+
+
+        tabLayout = findViewById(R.id.tab_Resources);
+
+//
+        FragmentPagerAdapter adapter = new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Fragment fragment = null;
+                if (position == 0) {
+                    fragment =  new NotesFragment();
+                } else if (position == 1) {
+                    fragment = new BooksFragment();
+                } else if (position == 2) {
+                    fragment = new QPaperFragment();
+                } else if (position == 3) {
+                    fragment = new PracticalsFragment();
+                }
+                return fragment;
+            }
+//
+            @Override
+            public int getCount() {
+                return 4;
+            }
+
+        };
+
+//        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+//                getSupportFragmentManager(), FragmentPagerItems.with(this)
+//                .add("Notes", NotesFragment.class)
+//                .add("Books", BooksFragment.class)
+//                .add("Q-Paper", QPaperFragment.class)
+//                .add("Practicals", PracticalsFragment.class)
+//                .create());
+
+
+        viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(adapter);
+
+//        SmartTabLayout viewPagerTab = findViewById(R.id.view_pager_tab);
+//        viewPagerTab.setViewPager(viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setText("Notes");
+        tabLayout.getTabAt(1).setText("Books");
+        tabLayout.getTabAt(2).setText("Q-Papers");
+        tabLayout.getTabAt(3).setText("Practicals");
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
